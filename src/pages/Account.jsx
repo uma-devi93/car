71be24520f67a990269f3880e92b6dbf4f25a377
uma-devi93 <umadevirrj.93@gmail.com>
2026-06-09 12:02:09 +0530

    Add files via upload

---
+++ src/pages/Account.jsx
@@ -0,0 +1,8 @@
+export default function Account() {
+  return (
+    <div className="page-container">
+      <h1>My Account</h1>
+      <p style={{ color:"black"}}>Manage your bookings, profile, and payments from here.</p>
+    </div>
+  );
+}
