72f824e5f3fd41732282f94952b0d750853f9be0
unknown <umadevirrj.93@gmail.com>
2026-06-10 14:53:52 +0530

    Backend ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import carRoutes from "./routes/carRoutes.js";
-import otpRoutes from "./routes/otp.js"; 
+import otpRoutes from "./routes/otp.js";
 
 dotenv.config();
 
@@ -27,15 +27,19 @@ app.use((req, res, next) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/cars", carRoutes);
-app.use("/api/otp", otpRoutes); //  OTP API
+app.use("/api/otp", otpRoutes);
+
+// PORT
+const PORT = process.env.PORT || 5000;
 
 // DATABASE + SERVER
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(5000, () =>
-      console.log("Server running on port 5000")
-    );
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => console.error(err));
